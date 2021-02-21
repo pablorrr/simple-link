af152d2ec31a78da4ada903a9303ab111420b1dd
Paweł Kali <pkalisz451@gmail.com>
2021-02-21 13:28:53 +0100

    konwersja kodu na OOP

--- simple-paste-link.php
+++ simple-paste-link.php
@@ -16,127 +16,156 @@ if (!function_exists('__return_false')) {
     require_once(ABSPATH . '/wp-includes/functions.php');
 }
 
-
-function add_link_shortcode($add_link_shortcode)
+class Paste_Link_Plugin
 {
 
-    $options = get_option('plug_settings');
-    $url = $options['input_text_field_0'];
-    $color = $options['plug_select_field_0'];
+    private $classoptionplugin_options;
 
 
-    switch ($color) {
-        case '1':
-            $add_link_shortcode = '<div><div class="animate"><p><a style="color:red" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
-            break;
-        case '2':
-            $add_link_shortcode = '<div><div class="animate"><p><a style="color:blue" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
-            break;
-        case '3':
-            $add_link_shortcode = '<div><div class="animate"><p><a style="color:green" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
-            break;
-        case '4':
-            $add_link_shortcode = '<div><div class="animate"><p><a style="color:black" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
-            break;
+    public function __construct()
+    {
+
+        //rejestracja menuadmin
+        add_action('admin_menu', array($this, 'paste_link_add_admin_menu'));
+        //options init
+        add_action('admin_init', array($this, 'paste_link_settings_init'));
+        //shortcode
+        add_shortcode('paste', array($this, 'add_link_shortcode'));
+        //options table
+        $this->classoptionplugin_options = get_option('plug_settings');
+
+
     }
 
 
-    return $add_link_shortcode;
-}
+    public function add_link_shortcode($add_link_shortcode)
+    {
 
-add_shortcode('paste', 'add_link_shortcode');
-
-/**
- *
- * sposob dodawania shortcoda na gutenberg: najpierw dodaj widget  shortcode gutenbera  potem wklj [paste] i zachowaj
- */
-
-add_action('admin_menu', 'plug_add_admin_menu');
-add_action('admin_init', 'plug_setting_init');
+        $this->classoptionplugin_options = get_option('plug_settings');//sprubowac ususnac te linie
+        $url = $this->classoptionplugin_options['input_text_field_0'];
+        $color = $this->classoptionplugin_options['plug_select_field_0'];
 
 
-function plug_add_admin_menu()
-{
-
-    add_menu_page('Paste Link',
-        'Paste Link',
-        'manage_options',
-        'paste-link',
-        'plug_options_page');
-
-}
-
-
-function plug_setting_init()
-{
-
-    register_setting('pluginPage', 'plug_settings');
-
-    add_settings_section(
-        'plug_pluginPage_section',
-        __('Your section description', 'polish'),
-        '__return_false',
-        'pluginPage'
-    );
-
-    add_settings_field(
-        'plug_select_field_0',
-        __('Settings field description', 'polish'),
-        'plug_select_field_0_render',
-        'pluginPage',
-        'plug_pluginPage_section'
-    );
-    add_settings_field(
-        'input_text_field_0',
-        'Wklej tutaj swój link url',
-        'callback_text_field_0_render',
-        'pluginPage',
-        'plug_pluginPage_section'
-    );
-}
+        switch ($color) {
+            case '1':
+                $add_link_shortcode = '<div><div class="animate"><p><a style="color:red" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
+                break;
+            case '2':
+                $add_link_shortcode = '<div><div class="animate"><p><a style="color:blue" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
+                break;
+            case '3':
+                $add_link_shortcode = '<div><div class="animate"><p><a style="color:green" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
+                break;
+            case '4':
+                $add_link_shortcode = '<div><div class="animate"><p><a style="color:black" target="_blanket" href="' . esc_url($url) . '">' . esc_url($url) . '</a></p><br></div></div>';
+                break;
+        }
 
 
-function plug_select_field_0_render()
-{
-    $options = get_option('plug_settings');
-    ?>
-    <select name='plug_settings[plug_select_field_0]'>
-        <option value='1' <?php selected($options['plug_select_field_0'], 1); ?>>red color</option>
-        <option value='2' <?php selected($options['plug_select_field_0'], 2); ?>>blue color</option>
-        <option value='3' <?php selected($options['plug_select_field_0'], 3); ?>>green color</option>
-        <option value='4' <?php selected($options['plug_select_field_0'], 4); ?>>black color</option>
-    </select>
-
-    <?php
-
-}
+        return $add_link_shortcode;
+    }
 
 
-function callback_text_field_0_render()
-{
-    $options = get_option('plug_settings');
-    ?>
-    <textarea name='plug_settings[input_text_field_0]'
-              value='<?php echo $options['input_text_field_0']; ?>'><?php echo esc_url($options['input_text_field_0']); ?></textarea>
-    <?php
+    /**
+     *
+     * sposob dodawania shortcoda na gutenberg: najpierw dodaj widget  shortcode gutenbera  potem wklj [paste] i zachowaj
+     */
 
-}
 
-function plug_options_page()
-{
-    ?>
-    <form action='options.php' method='post'>
+    public function paste_link_add_admin_menu()
+    {
 
-        <h2>my_plugin_name</h2>
+        add_menu_page('Paste Link',
+            'Paste Link',
+            'manage_options',
+            'paste-link',
+            array($this, 'paste_link_options_page'), // function
+            'dashicons-admin-generic', // icon_url
+            3 // position
+        );
+
+    }
+
+    public function paste_link_options_page()
+    {
+        $this->classoptionplugin_options = get_option('plug_settings');
+        ?>
+        <div class='wrap'>
+            <?php settings_errors(); ?>
+            <form action='options.php' method='post'>
+                <?php
+                settings_fields('pluginPage');
+                do_settings_sections('pluginPage');
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+
+
+    public function paste_link_settings_init()
+    {
+
+        register_setting('pluginPage', 'plug_settings');
+
+        add_settings_section(
+            'plug_pluginPage_section',
+            __('Your section description', 'polish'),
+            '__return_false',
+            'pluginPage'
+        );
+
+        add_settings_field(
+            'plug_select_field_0',
+            __('Settings field description', 'polish'),
+            array($this, 'plug_select_field_0_render'),
+            'pluginPage',
+            'plug_pluginPage_section'
+        );
+        add_settings_field(
+            'input_text_field_0',
+            'Wklej tutaj swój link url',
+            array($this, 'callback_text_field_0_render'),
+            'pluginPage',
+            'plug_pluginPage_section'
+        );
+    }
+
+
+    function plug_select_field_0_render()
+    {
+        ?>
+        <select name='plug_settings[plug_select_field_0]'>
+            <option value='1' <?php selected($this->classoptionplugin_options['plug_select_field_0'], 1); ?>>red color
+            </option>
+            <option value='2' <?php selected($this->classoptionplugin_options['plug_select_field_0'], 2); ?>>blue
+                color
+            </option>
+            <option value='3' <?php selected($this->classoptionplugin_options['plug_select_field_0'], 3); ?>>green
+                color
+            </option>
+            <option value='4' <?php selected($this->classoptionplugin_options['plug_select_field_0'], 4); ?>>black
+                color
+            </option>
+        </select>
 
         <?php
-        settings_fields('pluginPage');
-        do_settings_sections('pluginPage');
-        submit_button();
-        ?>
 
-    </form>
-    <?php
+    }
+
+
+    function callback_text_field_0_render()
+    {
+
+        ?>
+        <textarea name='plug_settings[input_text_field_0]'
+                  value='<?php echo $this->classoptionplugin_options['input_text_field_0']; ?>'><?php echo esc_url($this->classoptionplugin_options['input_text_field_0']); ?></textarea>
+        <?php
+
+    }
+
 }
 
+$Paste_Link_Plugin = new Paste_Link_Plugin();
 ?>
